c08b680d73ef84873ef04fa6a1d149d3c9979dfc
artemternovenko <artem.inkaz@gmail.com>
2020-12-27 19:29:33 +0600

    LiveData, Transformations.map

--- app/src/main/java/com/example/mobiledeveloper/MainActivity.kt
+++ app/src/main/java/com/example/mobiledeveloper/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.mobiledeveloper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,44 +10,42 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    var liveDataString = MutableLiveData<String>()
-    var liveDataInt = MutableLiveData<Int>()
+    val  liveDataString = MutableLiveData<String>()
+    val  liveDataString2 = MutableLiveData<String>()
+    val  mediatorLiveData = MediatorLiveData<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-           liveDataString.value = "liveDataString"
-            liveDataInt.value = 1
+        liveDataString.value = "String"
+        liveDataString2.value = "String2"
 
-        testText.text =liveDataString.value + "string"
-        testText1.text =liveDataInt.value.toString() + "int"
+        mediatorLiveData.addSource(liveDataString) {
 
-        //кнопка по которой будет преобразование типов из String в Int
-       buttonSafe.setOnClickListener {
-           liveDataString = Transformations.map(liveDataInt){
-               it.toString()
-           } as MutableLiveData<String>
+            testText.text = it
+        }
 
-           liveDataString.observe(this, Observer {
-               testText.text=it
+        mediatorLiveData.addSource(liveDataString2) {
 
-           })
-       }
+            testText1.text = it
+        }
 
-        //кнопка по которой будет преобразование типов из Int в String
-//        buttonSafe.setOnClickListener {
-//            liveDataInt = Transformations.map(liveDataString){
-//                it.toInt()
-//            } as MutableLiveData<Int>
-//
-//            liveDataString.observe(this, Observer {
-//                testText.text=it
-//
-//            })
-//        }
+        mediatorLiveData.observe(this, Observer {
 
 
+        })
+
+        buttonSafe.setOnClickListener {
+            liveDataString.value = edit_text.text.toString()
+
+        }
+
+        buttonSafe2.setOnClickListener {
+            liveDataString2.value = edit_text.text.toString()
+
+        }
     }
 
 }
