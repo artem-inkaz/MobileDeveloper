7751affab91b1fe76e17997f94be443532760e17
artemternovenko <artem.inkaz@gmail.com>
2020-12-27 18:13:12 +0600

    LiveData

--- app/src/main/java/com/example/mobiledeveloper/MainActivity.kt
+++ app/src/main/java/com/example/mobiledeveloper/MainActivity.kt
@@ -3,31 +3,26 @@ package com.example.mobiledeveloper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.MutableLiveData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val getData = GetData()
+
+  //  private var string = "data"
+    //создали экземпляр класса LiveData
+    private  var liveDateString = MutableLiveData<String>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    //    lifecycle.addObserver(getData)
-        if (lifecycle.currentState== Lifecycle.State.INITIALIZED)
-            println("INITIALIZED")
-        if (lifecycle.currentState== Lifecycle.State.CREATED)
-            println("CREATED")
-
+        liveDateString.value= "Hell Live Data"
+    //    testText1.text= liveDateString.value
     }
 
-    override fun onResume() {
-      //  if (lifecycle.currentState== Lifecycle.State.STARTED)
-        if (lifecycle.currentState== Lifecycle.State.CREATED)
-        //    println("STARTED")
-            println("CREATED")
-        super.onResume()
-    }
 
 
 
