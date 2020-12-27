2d35072247b5943b1bf93c55abc32fc2b5fcf93b
artemternovenko <artem.inkaz@gmail.com>
2020-12-27 16:30:18 +0600

    Стандартный способ LifeCycle

--- app/src/main/java/com/example/mobiledeveloper/GetData.kt
+++ app/src/main/java/com/example/mobiledeveloper/GetData.kt
@@ -1,14 +1,21 @@
 package com.example.mobiledeveloper
 
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 
 class GetData: LifecycleObserver {
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun getData(){
         println("Get Data")
     }
-
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun sendData(){
         println("Send Data")
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    fun any(){
+        println("any")
     }
 }
--- app/src/main/java/com/example/mobiledeveloper/MainActivity.kt
+++ app/src/main/java/com/example/mobiledeveloper/MainActivity.kt
@@ -11,34 +11,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//получаем название метода откуда мы запускаемся
-        println(object :Any(){}.javaClass.enclosingMethod?.name)
+
+    //    lifecycle.addObserver(getData)
     }
 
-    override fun onStart() {
-        println(object :Any(){}.javaClass.enclosingMethod?.name)
-        getData.getData()
-        super.onStart()
-    }
 
-//    override fun onResume() {
-//        println(object :Any(){}.javaClass.enclosingMethod?.name)
-//        super.onResume()
-//    }
 
-//    override fun onPause() {
-//        println(object :Any(){}.javaClass.enclosingMethod?.name)
-//        super.onPause()
-//    }
 
-    override fun onStop() {
-        println(object :Any(){}.javaClass.enclosingMethod?.name)
-        getData.sendData()
-        super.onStop()
-    }
 
-//    override fun onDestroy() {
-//        println(object :Any(){}.javaClass.enclosingMethod?.name)
-//        super.onDestroy()
-//    }
+
+
+
+
+
 }
